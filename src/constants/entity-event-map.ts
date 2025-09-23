8116fcbe36021bf7f955a3a1eebd3336feeb058c
Sadiq53 <jafarussadiq098@gmail.com>
2025-09-23 12:45:56 +0530

    created even mapping

---
+++ src/constants/entity-event-map.ts
@@ -0,0 +1,108 @@
+export const Vendor = {
+    VendorCreated: 'vendor.created',
+    VendorUpdated: 'vendor.updated',
+    VendorDeleted: 'vendor.deleted'
+}
+
+export const RawMaterial = {
+    RawMaterialCreated: 'raw_material.created',
+    RawMaterialUpdated: 'raw_material.updated',
+    RawMaterialDeleted: 'raw_material.deleted'
+}
+
+export const RMPurchasing = {
+    RMPurchasingOrdered: 'rm_purchasing.ordered',
+    RMPurchasingUpdated: 'rm_purchasing.updated',
+    RMPurchasingDeleted: 'rm_purchasing.deleted',
+    RMPurchasingCancelled: 'rm_purchasing.cancelled',
+    RMPurchasingReceived: 'rm_purchasing.received',
+    RMPurchasingPending: 'rm_purchasing.pending'
+}
+
+export const PurchaseData = {
+    PurchaseDataCreated: 'purchase_data.created',
+    PurchaseDataUpdated: 'purchase_data.created'
+}
+
+export const Production = {
+    ProductionStarted: 'production.started',
+    ProductionUpdated: 'production.updated',
+    ProductionCompleted: 'production.completed',
+    ProductionQueue: 'production.queue',
+    ProductionFailed: 'production.failed'
+}
+
+export const Chamber = {
+    ChamberCreated: 'chamber.craeted',
+    ChamberUpdated: 'chamber.updated',
+    ChamberDeleted: 'chamber.deleted'
+}
+
+export const ChamberStock = {
+    ChamberStockCreated: 'chamber_stock.created',
+    ChamberStockUpdated: 'chamber_stock.updated',
+}
+
+export const Order = {
+    OrderReady: 'order.ready',
+    OrderDelivered: 'order.delivered',
+    OrderDispatched: 'order.dispatched',
+    OrderUpdated: 'order.updated',
+    OrderCancelled: 'order.cancelled',
+}
+
+export const Truck = {
+    TruckCreated: 'truck.created',
+    TruckUpdated: 'truck.updated',
+    TruckDeleted: 'truck.deleted',
+    TruckAssigned: 'truck.assigned'
+}
+
+export const Lane = {
+    LaneCreated: 'lane.created',
+    LaneUpdated: 'lane.updated',
+    LaneOccupied: 'lane.occupied',
+    LaneFreed: 'lane.freed',
+}
+
+export const Contractor = {
+    ContractorCreated: 'contractor.created',
+    ContractorDeleted: 'contractor.deleted',
+}
+
+export const Product = {
+    ProductCreated: 'product.created',
+    ProductUpdated: 'product.updated',
+    ProductDeleted: 'product.deleted',
+    ProductDeactivated: 'product.deactivated',
+}
+
+export const Packet = {
+    PacketCreated: 'packet.created',
+    PacketUpdated: 'packet.updated',
+} 
+
+export const Packaging = {
+    PackagingCreated: 'packaging.created',
+    PackagingUpdated: 'packaging.updated',
+    PackagingDeleted: 'packaging.deleted',
+    PackagingAdded: 'packaging.added',
+    PackagingDeducted: 'packaging.deducted'
+} 
+
+export const OrderProduct = {
+    OrderProductCreated: 'order_product.created',
+    OrderProductUpdated: 'order_product.updated'
+}
+
+export const Challan = {
+    ChallanCreated: 'challan.created',
+    ChallanUpdated: 'challan.updated',
+    ChallanDeleted: 'challan.deleted'
+}
+
+export const Image = {
+    ImageCreated: 'image.created',
+    ImageUpdated: 'image.updated',
+    ImageDeleted: 'image.deleted'
+}
